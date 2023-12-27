fd5a5d777e44b8af60dec1595b981f4e6815118b
Vaibhav Surana <vaibhav.surana@hackerrank.com>
2023-12-27 18:10:25 +0530

    revert setText change

--- src/monaco-adapter.ts
+++ src/monaco-adapter.ts
@@ -398,7 +398,7 @@ export class MonacoAdapter implements IEditorAdapter {
 
   setInitiated(init: boolean): void {
     // Perfomance boost on clearing editor after network calls (do not directly setValue or EOL will get reset and break sync)
-    console.log('this.setText("");');
+    this.setText("");
     this._initiated = init;
   }
 
